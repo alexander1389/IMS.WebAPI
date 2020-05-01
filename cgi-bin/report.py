b1e3b7a556cefd2fe06a41109b3bc3f65cfe6d3c
Александр <alexander@MacBook-Pro-Aleksandr.local>
2020-05-02 00:47:30 +0300

    #001
    get_last_report implementation
    minor improvements

--- cgi-bin/report.py
+++ cgi-bin/report.py
@@ -1,19 +1,21 @@
 import sys
 import time
 
-from os import listdir
+from os import listdir, stat
 from os.path import isfile, join, splitext, getctime, getsize, basename
 from shutil import copyfileobj
 from zipfile import ZipFile
 
 from bad_request import bad_request
-from utild import validate_dt
-
+from utils import validate_dt
+from datetime import datetime
 
 DEFAULT_EXT = '.xml'
 
 def report(**kwargs):
-    reply = get_reports_by_date('/tmp', '200301', '.pdf')
+    #reply = get_reports_by_date('/tmp', '200301', '.pdf')
+
+    reply = get_last_report('/tmp', '.txt')
 
     return reply
 
@@ -23,10 +25,18 @@ def report(**kwargs):
 
 # -------------------------------------------
 # TODO: doc
+def get_ctime(path):
+    st_info = stat(path)
+    try:
+        t = st_info.st_birthtime
+    except AttributeError:
+        t = st_info.st_mtime
+
+    return datetime.fromtimestamp(t).strftime("%y%m%d%H%M%S")
+
 def get_replist(path, ext = DEFAULT_EXT):
-    return { f: time.strftime("%y%m%d%H%M%S", time.gmtime(getctime(f)))
-        for f in (join(path, f) for f in listdir(path)) 
-        if isfile(f) and splitext(f)[1] == ext }
+    return { f: get_ctime(f) for f in (join(path, f) for f in listdir(path)) 
+             if isfile(f) and splitext(f)[1] == ext }
 
 def filter_by_date(lst, date):
     date = date.replace('_', '')
@@ -63,7 +73,26 @@ def get_reports_by_date(path, date, ext = DEFAULT_EXT):
 # -------------------------------------------
 
 
+# TODO: fix filename
+def get_last_report(path, ext = DEFAULT_EXT):
+    lst = get_replist(path, ext)
+    if not lst:
+        bad_request(404, msg='No Reports')
 
+    last_report = sorted(lst.items(), key=lambda x: (x[1], x[0]), reverse=True)[0][0]
+
+    print('Cache-Control: no-cache')
+    print('Cache-Control: no-store')
+    print('Content-Disposition: attachment; filename="%s"' % last_report)
+    print('Content-Transfer-Encoding: binary')
+    print('Content-Length: %d' % getsize(last_report))
+    print('Content-Type: application/octet-stream')
+    print(flush=True)
+    
+    with open(last_report,'rb') as file:
+        copyfileobj(file, sys.stdout.buffer)
+
+    #print(last_report)
 
 
 
@@ -73,7 +102,7 @@ def get_reports_by_date(path, date, ext = DEFAULT_EXT):
 TEST_PATH = '/tmp'
 if __name__ == '__main__':
     print("TEST...")
-
+    """
     d = get_replist(TEST_PATH, '.pdf')
     for k, v in d.items():
         print("%s - %s" % (k, v))
@@ -84,3 +113,5 @@ if __name__ == '__main__':
 
     rep = get_reports_by_date(TEST_PATH, '2004', '.pdf')
     print(rep)
+    """
+    get_last_report(TEST_PATH, '.txt')
