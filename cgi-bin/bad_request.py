e2ef748f48b1a611e4753613f977494e941cabcf
Александр <alexander@MacBook-Pro-Aleksandr.local>
2020-05-02 15:54:08 +0300

    #006
    bad_request.py fixed according to the PEP8

--- cgi-bin/bad_request.py
+++ cgi-bin/bad_request.py
@@ -1,10 +1,20 @@
 import sys
 
+
 def bad_request(code=404, str='Not Found', msg=''):
-    print('Status: %d %s' % (code, msg))
+    """ Send error HTTP response
+
+    :param code: The error code (optional (default=404))
+    :type code: number
+    :param str: The error message (optional (default='Not Found'))
+    :type str: str
+    :param msg: The extended error message in the body
+                    of HTTP response (optional (default=''))
+    """
+    print('Status: %d %s' % (code, str))
     print('Content-Type: text/html; charset=utf-8')
     print('Content-Length: %d' % len(msg))
-    print(flush=True) 
+    print(flush=True)
 
     print(msg)
 
