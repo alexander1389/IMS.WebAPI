10eec9258050388d146ed8c0dfb4eab28b590e57
Александр <alexander@MacBook-Pro-Aleksandr.local>
2020-05-02 12:53:38 +0300

    #002
    docstrings added

--- cgi-bin/report.py
+++ cgi-bin/report.py
@@ -1,14 +1,14 @@
 import sys
 import time
 
+from datetime import datetime
 from os import listdir, stat
-from os.path import isfile, join, splitext, getctime, getsize, basename
+from os.path import isfile, join, splitext, getsize, basename
 from shutil import copyfileobj
 from zipfile import ZipFile
 
 from bad_request import bad_request
 from utils import validate_dt
-from datetime import datetime
 
 DEFAULT_EXT = '.xml'
 
@@ -24,8 +24,14 @@ def report(**kwargs):
 
 
 # -------------------------------------------
-# TODO: doc
 def get_ctime(path):
+    """ Get file creation time 
+
+    :param path: The file location path
+    :type path: str
+    :returns: a file creation time (or last modified time if ctime getting fails on *nix)
+    :rtype: str
+    """
     st_info = stat(path)
     try:
         t = st_info.st_birthtime
@@ -35,10 +41,28 @@ def get_ctime(path):
     return datetime.fromtimestamp(t).strftime("%y%m%d%H%M%S")
 
 def get_replist(path, ext = DEFAULT_EXT):
+    """ Get a list of reports (non-recursively)
+
+    :param path: The location of reports
+    :type path: str
+    :param ext: The extension of reports files (optional (default=DEFAULT_EXT))
+    :type ext: str
+    :returns: a dictionary containing pairs of absolute filename and creation time of the report
+    :rtype: dict
+    """
     return { f: get_ctime(f) for f in (join(path, f) for f in listdir(path)) 
              if isfile(f) and splitext(f)[1] == ext }
 
 def filter_by_date(lst, date):
+    """ Filter reports list by date
+
+    :param lst: Reports dictionary
+    :type lst: dict
+    :param date: Date to filter by
+    :type date: str
+    :returns: list of absolute filenames of reports corresponding to the date
+    :rtype: list
+    """
     date = date.replace('_', '')
 
     if not lst or not date: 
@@ -50,6 +74,15 @@ def filter_by_date(lst, date):
     return [k for k, v in lst.items() if v[:len(date)] == date]
 
 def get_reports_by_date(path, date, ext = DEFAULT_EXT):
+    """ Get reports filtered by date in an HTTP response
+
+    :param path: The location of reports
+    :type path: str
+    :param date: Date to get reports by
+    :type date: str
+    :param ext: The extension of reports files (optional (default=DEFAULT_EXT))
+    :type ext:str
+    """
     reports = filter_by_date(get_replist(path, ext), date)
     if not reports:
         bad_request(404, msg='No Reports')
@@ -62,7 +95,7 @@ def get_reports_by_date(path, date, ext = DEFAULT_EXT):
 
     print('Cache-Control: no-cache')
     print('Cache-Control: no-store')
-    print('Content-Disposition: inline; filename="%s"' % arch_name)
+    print('Content-Disposition: attachment; filename="%s"' % arch_name)
     print('Content-Transfer-Encoding: binary')
     print('Content-Length: %d' % getsize(arch_name))
     print('Content-Type: application/zip')
@@ -70,20 +103,25 @@ def get_reports_by_date(path, date, ext = DEFAULT_EXT):
     
     with open(arch_name,'rb') as zipfile:
         copyfileobj(zipfile, sys.stdout.buffer)
-# -------------------------------------------
 
-
-# TODO: fix filename
 def get_last_report(path, ext = DEFAULT_EXT):
+    """ Get last created report in an HTTP response
+
+    :param path: The location of report
+    :type path: str
+    :param ext: The extension of report file (optional (default=DEFAULT_EXT))
+    :type ext:str
+    """
     lst = get_replist(path, ext)
     if not lst:
         bad_request(404, msg='No Reports')
 
     last_report = sorted(lst.items(), key=lambda x: (x[1], x[0]), reverse=True)[0][0]
+    report_name = basename(last_report)
 
     print('Cache-Control: no-cache')
     print('Cache-Control: no-store')
-    print('Content-Disposition: attachment; filename="%s"' % last_report)
+    print('Content-Disposition: attachment; filename="%s"' % report_name)
     print('Content-Transfer-Encoding: binary')
     print('Content-Length: %d' % getsize(last_report))
     print('Content-Type: application/octet-stream')
@@ -92,11 +130,8 @@ def get_last_report(path, ext = DEFAULT_EXT):
     with open(last_report,'rb') as file:
         copyfileobj(file, sys.stdout.buffer)
 
-    #print(last_report)
+# -------------------------------------------
 
-
-
-# TODO: last cmd part
 
 
 TEST_PATH = '/tmp'
